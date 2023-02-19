d0e135b3bc4546cc9c52fbc0bd95bc3a6280d3b4
isthatsilva <111096274+isthatsilva@users.noreply.github.com>
2023-02-19 13:43:57 -0500

    update resume

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -3,7 +3,7 @@ import PDF from "../../assets/Resume.pdf";
 
 const Resume = () => {
     return (
-        <section className='my-5 text-background'>
+        <section>
             <embed id="Resume" src={PDF} width="100%" height="550" />
         </section>
     )
